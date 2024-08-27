f237ef364d51ad34d0dc1a930b68518f7bb160b4
Nadiia Yuce <soyka9616@gmail.com>
2024-08-28 00:48:23 +0200

    add delete animation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,27 @@
 import { FaUser } from "react-icons/fa6";
 import { FaPhone } from "react-icons/fa6";
+import { useState } from "react";
 import css from "./Contact.module.css";
+import clsx from "clsx";
 
 export default function Contact({ contact: { name, number, id }, onDelete }) {
+  const [isRemoving, setIsRemoving] = useState(false);
+
+  const handleRemove = () => {
+    setIsRemoving(true);
+    setTimeout(() => {
+      onDelete(id);
+    }, 500);
+  };
+
   return (
-    <div className={css.container}>
+    <div
+      className={clsx(
+        css.card,
+        "animate__animated",
+        isRemoving && "animate__fadeOut"
+      )}
+    >
       <div className={css.wrap}>
         <FaUser size={16} color="rgb(97, 76, 150)" />
         <p className={css.text}>{name}</p>
@@ -15,7 +32,7 @@ export default function Contact({ contact: { name, number, id }, onDelete }) {
       </div>
       {/* при події onClick викликається коллбек onDelete, якому передається поточне значеня id, по якому має видалятися обʼєкт */}
       {/* атрибут id ф-ї onDelete піднімається до початкової ф-ї removeContact в App  */}
-      <button type="button" className={css.delete} onClick={() => onDelete(id)}>
+      <button type="button" className={css.delete} onClick={handleRemove}>
         Delete
       </button>
     </div>
